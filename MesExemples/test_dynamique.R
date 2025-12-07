9944bd07a68bb03299feab76691aaf3e14cd2de2
flaviebertrand <flavie.bertrand2106@gmail.com>
2025-12-07 20:19:51 +0100

    Exemple avec les modifications de l'algo dynamique en R

--- MesExemples/test_dynamique.R
+++ MesExemples/test_dynamique.R
@@ -1,53 +1,23 @@
 ### TEST RANKING DYNAMIQUE ###
 
 # EXEMPLE MAX
-# Exemple de données
+
+# Données d'exemple
 data <- data.frame(
-  id = 1:5,
-  score = c(10, 9, 8, 7, 6),
-  groupes = c("A", "B", "A,B", "B", "A"),
-  stringsAsFactors = FALSE
+  id = 1:10,
+  score = c(1, 9, 8, 7, 6, 5, 4, 3, 2, 10),
+  groupes = c("C", "A", "B", "B", "C", "A,B", "C", "A", "B", "A")
 )
 
-# Contraintes
-k <- 3
-max_par_groupe <- list(A = 2, B = 2)
+# Contraintes: max 2 éléments du groupe A, 2 du groupe B, 1 du groupe C
+max_par_groupe <- list(A = 2, B = 2, C = 1)
 
-# Test Max
-res <- ranking_max(data, k, max_par_groupe)
-print(res)
+# Poids décroissants pour les positions (position 1 = poids 5, position 2 = poids 4, etc.)
+poids <- c(5, 4, 3, 2, 1)
 
-#####################################################
-# EXEMPLE MIN
-data <- data.frame(
-  id = 1:5,
-  score = c(10, 9, 8, 7, 6),
-  groupes = c("A", "B", "A,B", "B", "A"),
-  stringsAsFactors = FALSE
-)
+# Résolution
+result <- ranking_max(data, k = 5, max_par_groupe, poids_positions = poids)
 
-# Contraintes
-k <- 4
-min_par_groupe <- list(A = 2, B = 1)
-
-# Test Min
-ranking_min(data, k, min_par_groupe)
-
-####################################################
-# EXEMPLE MIN AND MAX
-
-# Exemple de données
-data <- data.frame(
-  id = 1:6,
-  score = c(10, 9, 8, 7, 6, 5),
-  groupes = c("A", "B", "A,B", "B", "A", "A"),
-  stringsAsFactors = FALSE
-)
-
-# Contraintes
-k <- 4
-min_par_groupe <- list(A = 2, B = 1)
-max_par_groupe <- list(A = 3, B = 2)
-
-# Test max and min
-ranking_minmax(data, k, min_par_groupe, max_par_groupe)
+print("Éléments sélectionnés:")
+print(result$selected_items)
+print(paste("Score total:", result$best_score))
